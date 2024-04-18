31d8ab821f080d6d8360942e5e358aa232049c60
Moon <43591014+MasayukiNagai@users.noreply.github.com>
2024-04-18 14:53:29 -0400

    Update run_command.sh

--- slack_notifier/run_command.sh
+++ slack_notifier/run_command.sh
@@ -9,17 +9,20 @@ start=`date +%s`
 $COMMAND
 
 if [ $? -ne 0 ]; then
-  msg="Your job returned non-zero status.\n"
+  job_msg="Your job returned non-zero status."
 else
-  msg="Your job was successfully done.\n"
+  job_msg="Your job was successfully done."
 fi
 
 end=`date +%s`
 runtime=$((end-start))
+min=$((run_time / 60))
+sec=$((run_time % 60))
 
-msg+="CMD: ${COMMAND}\n"
+msg="CMD: ${COMMAND}\n"
 msg+="DATE: ${DATE}\n"
 msg+="HOST: ${HOST}\n"
-msg+="Run time: ${runtime} (/s)"
+msg+="Run time: ${min} (min) ${sec} (sec)\n"
+msg+="Comment: $job_msg"
 
 echo -e ${msg} | slack_webhook.sh
